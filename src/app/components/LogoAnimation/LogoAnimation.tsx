330763ca2124265539af8737f9d8ab64945e4470
ethanw03 <ethanwong98@yahoo.ca>
2024-02-26 01:27:41 -0800

    small change

--- src/app/components/LogoAnimation/LogoAnimation.tsx
+++ src/app/components/LogoAnimation/LogoAnimation.tsx
@@ -6,7 +6,7 @@ import logoImage from '../../../assets/Yello_Icon_Black.png';
 const LogoAnimation: React.FC = () => {
   return (
     <div className='bg-white w-full h-lvh'>
-      <div className='flex justify-center h-lvh items-center'>
+      <div className='flex justify-center w-full h-lvh items-center'>
       <Image src={logoImage} alt='logo' className='flex h-fit'></Image>
       </div>
     </div>
