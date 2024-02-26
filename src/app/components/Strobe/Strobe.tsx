032817abef11cfc9eee9bc70c3e413c17fc168f7
ethanw03 <ethanwong98@yahoo.ca>
2024-02-26 01:50:38 -0800

    restart strobe page style to center

--- src/app/components/Strobe/Strobe.tsx
+++ src/app/components/Strobe/Strobe.tsx
@@ -4,8 +4,10 @@ import { motion, useAnimation } from 'framer-motion';
 type StrobeProps = {
 };
 
-const Strobe: React.FC<StrobeProps> = ({}) => {
-  
-};
+const Strobe: React.FC<StrobeProps> = ({}) => 
+
+<div className='w-full h-lvh'>
+  <div className='flex justify-center text-9xl font-clash h-lvh items-center'>YELLO</div>
+</div>;
 
 export default Strobe;
