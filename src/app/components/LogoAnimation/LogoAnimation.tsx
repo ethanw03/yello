45ecbd35154f1e910b4f24ba1d4ba476ed647cb9
ethanw03 <ethanwong98@yahoo.ca>
2024-02-27 00:23:34 -0800

    more styling and changed animation background to dark

--- src/app/components/LogoAnimation/LogoAnimation.tsx
+++ src/app/components/LogoAnimation/LogoAnimation.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
-import logoImage from '../../../assets/Yello_Icon_Black.png';
+import logoImage from '../../../assets/Yello-Icon-White.png';
 
 const LogoAnimation: React.FC = () => {
   return (
-    <div className='bg-brand-white w-full h-screen flex justify-center items-center overflow-hidden mx-auto my-auto'>
+    <div className='bg-brand-black w-full h-screen flex justify-center items-center overflow-hidden mx-auto my-auto'>
      
       <div className='w-[300px] h-[300px] md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px]'> 
         <Image src={logoImage} alt='logo' layout='responsive' className='rotate fill-brand-yellow'/>
